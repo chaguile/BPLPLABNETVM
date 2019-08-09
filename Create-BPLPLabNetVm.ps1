5f8187fa18d7b9bb6e46fa47db4a8a76d2a4c864
chaguile <41484612+chaguile@users.noreply.github.com>
2019-08-09 17:12:41 -0400

    Update Create-BPLPLabNetVm.ps1

--- Create-BPLPLabNetVm.ps1
+++ Create-BPLPLabNetVm.ps1
@@ -31,11 +31,18 @@ $SubnetName = "NetLabSubnet"
 $SubnetAddressPrefix = "192.168.0.0/24"
 $VnetAddressPrefix = "192.168.0.0/24"
 
+write-host "Creating the Resource Group" $ResourceGroupName -ForegroundColor Green
 New-AzResourceGroup -Name $ResourceGroupName -Location $locationName
+
+write-host "Creating the Virtual Network. Some warnings are expected" -ForegroundColor Green
 $SingleSubnet = New-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
 $Vnet = New-AzVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $LocationName -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
+
+write-host "Creating the Public IP and the Network card" -ForegroundColor Green
 $PIP = New-AzPublicIpAddress -Name $PublicIPAddressName -ResourceGroupName $ResourceGroupName -Location $LocationName -AllocationMethod Dynamic
 $NIC = New-AzNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
+
+write-host "Creating the Virtual Machine" -ForegroundColor Green
 $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
 $VirtualMachine = New-AzVMConfig -VMName $VMName -VMSize $VMSize
 $VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
@@ -43,4 +50,4 @@ $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
 $VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsDesktop' -Offer 'Windows-10' -Skus '19h1-pro' -Version latest
 New-AzVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $VirtualMachine -Verbose
 
-write-host "This ends the tasks, please check your vm on http://portal.azure.com" -ForegroundColor Green
+write-host "This ends the tasks. Please check your new VM" $ComputerName " on http://portal.azure.com" -ForegroundColor Green
